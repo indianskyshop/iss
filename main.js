3177d5f941523f46e249b026c46daf602e5c498d
indianskyshop <91629299+indianskyshop@users.noreply.github.com>
2022-01-01 20:31:49 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+  <script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
