e59edef4972cfe236a67377988f3cd16587554e7
indianskyshop <91629299+indianskyshop@users.noreply.github.com>
2022-01-03 22:32:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -28,17 +40,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
 }
 
 function srch() {
