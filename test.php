c42b9bc6f33a7e713c35a9008a258a073592de3b
indianskyshop <91629299+indianskyshop@users.noreply.github.com>
2022-01-24 14:58:42 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php echo 'if you want to serve PHP code in XHTML or XML documents,
+                use these tags'; ?>
+<html>
+  <head>
+    <title>iss test</title>
+  </head>
+  <body>
+    <p>testinggggg haiiii</p>
+    <a>parvvvv</a>
+  </body>
+</html>
